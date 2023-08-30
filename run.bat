70087acccf0a6652422c9744a32cb93f20fd8af4
Alejandro Alzate <alejandroalzatesanchez@gmail.com>
2023-08-29 19:40:04 -0500

    Adding lame crash warning

--- run.bat
+++ run.bat
@@ -1,4 +1,13 @@
+@echo off
+echo Starting the server app...
 start /min KeystrokeServer.exe
+echo *Don't touch the keyboard or mouse before okw is opened*
+echo since this spams the server and gets DoS'ed making it
+echo eating 99% of your cpu makin the pc laggy and very
+echo unresposive for about a second or two before the
+echo task management catch it and makes it crash 
+echo I dont really know how to fix it and its cringy and lame
+echo that i had to put a note about it in here.
 timeout /t 3
 "C:\Program Files\LOVE\love.exe" .\
 exit /b
